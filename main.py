d4df719aa663c07a757f553e3790b233f1a2ba57
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-18 23:36:25 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,31 +1,92 @@
-def eng_uzun_soz(matn):
-    sozlar = matn.split()
-    eng_uzun = sozlar[0]
-    for soz in sozlar:
-        if len(soz) > len(eng_uzun):
-            eng_uzun = soz
-    return eng_uzun
+class Author:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
-matn = "Bu mening birinchi dasturim"
-print(eng_uzun_soz(matn))
+class Post:
+    def __init__(self, title, content, author):
+        self.title = title
+        self.content = content
+        self.author = author
+        self.comments = []
 
-def eng_uzun_soz2(matn):
-    sozlar = matn.split()
-    sozlar.sort(key=len, reverse=True)
-    return sozlar[0]
+    def add_comment(self, comment):
+        self.comments.append(comment)
 
-matn = "Bu mening birinchi dasturim"
-print(eng_uzun_soz2(matn))
+class Comment:
+    def __init__(self, content, author):
+        self.content = content
+        self.author = author
 
-def eng_uzun_soz3(matn):
-    sozlar = matn.split()
-    max_uzunlik = 0
-    eng_uzun = ""
-    for soz in sozlar:
-        if len(soz) > max_uzunlik:
-            max_uzunlik = len(soz)
-            eng_uzun = soz
-    return eng_uzun
+class BlogSystem:
+    def __init__(self):
+        self.posts = []
+        self.authors = []
 
-matn = "Bu mening birinchi dasturim"
-print(eng_uzun_soz3(matn))
+    def add_post(self, post):
+        self.posts.append(post)
+
+    def add_author(self, author):
+        self.authors.append(author)
+
+    def print_all_posts(self):
+        for post in self.posts:
+            print(post.title)
+            print(post.content)
+            print("Author: " + post.author.name)
+            for comment in post.comments:
+                print("Comment: " + comment.content + " by " + comment.author.name)
+
+class BlogApp:
+    def __init__(self):
+        self.blog_system = BlogSystem()
+
+    def run(self):
+        while True:
+            print("1. Create author")
+            print("2. Create post")
+            print("3. Add comment")
+            print("4. Print all posts")
+            print("5. Exit")
+            choice = input("Enter your choice: ")
+            if choice == "1":
+                name = input("Enter author name: ")
+                email = input("Enter author email: ")
+                author = Author(name, email)
+                self.blog_system.add_author(author)
+            elif choice == "2":
+                title = input("Enter post title: ")
+                content = input("Enter post content: ")
+                author_name = input("Enter author name: ")
+                for author in self.blog_system.authors:
+                    if author.name == author_name:
+                        post = Post(title, content, author)
+                        self.blog_system.add_post(post)
+                        break
+                else:
+                    print("Author not found")
+            elif choice == "3":
+                post_title = input("Enter post title: ")
+                for post in self.blog_system.posts:
+                    if post.title == post_title:
+                        content = input("Enter comment content: ")
+                        author_name = input("Enter author name: ")
+                        for author in self.blog_system.authors:
+                            if author.name == author_name:
+                                comment = Comment(content, author)
+                                post.add_comment(comment)
+                                break
+                        else:
+                            print("Author not found")
+                        break
+                else:
+                    print("Post not found")
+            elif choice == "4":
+                self.blog_system.print_all_posts()
+            elif choice == "5":
+                break
+            else:
+                print("Invalid choice")
+
+app = BlogApp()
+app.run()
